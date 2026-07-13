be4b514d8effb9ad50a5b11480b830c468671fbc
shLiao <101708809+shliao-e@users.noreply.github.com>
2026-07-13 17:10:05 +0800

    Update eval_rgb.py

--- eval_rgb.py
+++ eval_rgb.py
@@ -8,7 +8,7 @@ from torch.utils.data import DataLoader
 from src.datasets.test_datasets import CTS,CTS_yuv
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import save_image
-from src.models.video_simplergb import Multi_DMC
+from src.models.video_simplergb import Multi_DMC,DMC
 from src2.models.image_model import IntraNoAR
 # from src.models.video_net_dmc import DMC
 import csv
@@ -24,15 +24,15 @@ parser = argparse.ArgumentParser(description="Example training script")
 
 
 parser.add_argument("--p_model", type=str, default='Argb_checkpoints/finetune/')
-parser.add_argument('--model_name', type=str, default="finetune2_7", help='models_name')
+parser.add_argument('--model_name', type=str, default="finetune3_8", help='models_name')
 parser.add_argument("--i_model", type=str, default='./cvpr2023_image_psnr.pth.tar')
-parser.add_argument('--gop_size', type=int,default=40)
+parser.add_argument('--gop_size', type=int,default=50)
 parser.add_argument('--lambda', dest='lmbda', type=int, default=4)
 
 parser.add_argument('--cuda_id', type=int, default= 0, help='Use cuda')
 parser.add_argument('--save_dir', type=str, default="./", help='Path to save models')
 # parser.add_argument('--test_class', type=str, default="ClassB", help='Path to save models')
-parser.add_argument('--test_class', type=str, default="huawei-test2", help='Path to save models')
+parser.add_argument('--test_class', type=str, default="DJL", help='Path to save models')
 args = parser.parse_args()
 
 
@@ -85,6 +85,7 @@ def test_one_epoch(video_net, image_net, test_dataloader,  epoch, device = None,
     sum_bpp = 0
     sum_kbps = 0 
     cnt = 0
+    down_factor = 1.0
     for b, frames, in enumerate(test_dataloader):
         seq_lpips = 0
         seq_psnr_avg = 0
@@ -115,9 +116,9 @@ def test_one_epoch(video_net, image_net, test_dataloader,  epoch, device = None,
                     i_q_index = {'1024':3, '128':2, '64':1, '16': 0, '8': 0, '4': 0, '2': 0, '1': 0}
                     q_index = i_q_index[str(args.lmbda)]
                     ##执行I帧分辨率下采样
-                    down_factor = 1/2
-                    cur_frame_resize = F.interpolate(cur_frame,scale_factor=down_factor,mode="bilinear")
-                    padding_l_d, padding_r_d, padding_t_d, padding_b_d = get_padding_size(int(h*down_factor), int(w*down_factor), 64)
+                    down_factor_i = 1/2
+                    cur_frame_resize = F.interpolate(cur_frame,scale_factor=down_factor_i,mode="bilinear")
+                    padding_l_d, padding_r_d, padding_t_d, padding_b_d = get_padding_size(int(h*down_factor_i), int(w*down_factor_i), 64)
                     cur_frame_resize_pad = torch.nn.functional.pad(
                             cur_frame_resize,
                             (padding_l_d, padding_r_d, padding_t_d, padding_b_d),
@@ -126,15 +127,9 @@ def test_one_epoch(video_net, image_net, test_dataloader,  epoch, device = None,
                     ref_frame = result['x_hat'].detach()
                      #************指标计算**************
                     recon_frame = F.pad(ref_frame, (-padding_l_d, -padding_r_d, -padding_t_d, -padding_b_d))
-                    recon_frame =  F.interpolate(recon_frame ,scale_factor=1/down_factor,mode="bilinear")
+                    recon_frame =  F.interpolate(recon_frame ,scale_factor=1/down_factor_i,mode="bilinear")
                     recon_frame = recon_frame.clamp_(0.,1.)
-                    save_image(recon_frame, os.path.join(img_save_dir, f"seq{b}_frame{frame_idx}.png"))
-
-                    ref_frame = torch.nn.functional.pad(
-                                                        recon_frame,
-                                                        (padding_l, padding_r, padding_t, padding_b),
-                                                        mode="replicate",)
-
+                    # save_image(recon_frame, os.path.join(img_save_dir, f"seq{b}_frame{frame_idx}.png"))
                     # 计算Y分量的MSE和PSNR
                     intra_mse = F.mse_loss(recon_frame, cur_frame).item()
                     intra_psnr = 10 * (np.log(1 * 1 / intra_mse) / np.log(10))
@@ -156,6 +151,13 @@ def test_one_epoch(video_net, image_net, test_dataloader,  epoch, device = None,
                     sum_mse += intra_mse
                     sum_psnr += intra_psnr
                     cnt +=1
+                    ### 为p帧参考做形变
+                    ref_frame =  F.interpolate(recon_frame,scale_factor=down_factor,mode="bilinear")
+                    padding_l_d, padding_r_d, padding_t_d, padding_b_d = get_padding_size(int(h*down_factor), int(w*down_factor), 64)
+                    ref_frame = torch.nn.functional.pad(
+                            ref_frame,
+                            (padding_l_d, padding_r_d, padding_t_d, padding_b_d),
+                            mode="replicate",)
                     dpb = {
                         'ref_frame': ref_frame,
                         'ref_feature': ref_feature,
@@ -163,8 +165,14 @@ def test_one_epoch(video_net, image_net, test_dataloader,  epoch, device = None,
                         'ref_feature_L': None,
                         } 
                     continue
-                idx_ = frame_idx % 8
-                result = video_net.forward_one_frame(cur_frame_pad,dpb,idx_,True)               
+                idx_ = frame_idx % 4
+                cur_frame_resize = F.interpolate(cur_frame,scale_factor=down_factor,mode="bilinear")
+                padding_l_d, padding_r_d, padding_t_d, padding_b_d = get_padding_size(int(h*down_factor), int(w*down_factor), 64)
+                cur_frame_resize_pad = torch.nn.functional.pad(
+                            cur_frame_resize,
+                            (padding_l_d, padding_r_d, padding_t_d, padding_b_d),
+                            mode="replicate",)
+                result = video_net.forward_one_frame(cur_frame_resize_pad,dpb,idx_,True)               
                 if frame_idx >0 :
                     ref_feature = result['dpb']['ref_feature'].detach()
                     ref_y = result['dpb']['ref_y'].detach()
@@ -174,25 +182,26 @@ def test_one_epoch(video_net, image_net, test_dataloader,  epoch, device = None,
                         'ref_feature': ref_feature,
                         'ref_y':ref_y,
                         }   
-                # if frame_idx % 20 ==0:
+                # if frame_idx %  ==0:
                 #     dpb = {
                 #     'ref_frame': ref_frame,
-                #         'ref_feature': ref_feature,
+                #         'ref_feature': None,
                 #         'ref_y':None,
-                #         'ref_feature_L': ref_feature_L,
                 #     } 
                 
                #************指标计算**************
-                recon_frame = F.pad(ref_frame, (-padding_l, -padding_r, -padding_t, -padding_b))
+                recon_frame = F.pad(ref_frame, (-padding_l_d, -padding_r_d, -padding_t_d, -padding_b_d), mode="replicate",)
+                recon_frame =  F.interpolate(recon_frame,scale_factor=1/down_factor,mode="bilinear")
+
                 inter_mse =  F.mse_loss(recon_frame, cur_frame).item()
                 inter_psnr =10 * (np.log(1 * 1 / inter_mse) / np.log(10))
                 lpips_loss = loss_fn(recon_frame,cur_frame).mean().item()
                
-                save_image(recon_frame, os.path.join(img_save_dir, f"seq{b}_frame{frame_idx}.png"))
+                # save_image(recon_frame, os.path.join(img_save_dir, f"seq{b}_frame{frame_idx}.png"))
                 # 计算平均PSNR（可以加权，这里简单平均）
                 seq_lpips+= lpips_loss
                 # inter_psnr = (6*psnr_y + psnr_u + psnr_v) / 8
-                bpp = result['bpp'].item()
+                bpp = result['bits'].item()/h/w
                 seq_bpp += bpp
                 seq_psnr_avg += inter_psnr
                 seq_mse += inter_mse
@@ -231,8 +240,8 @@ def eval():
     f = open(file_dir, 'w', encoding= 'utf-8')
     csv_writer = csv.writer(f)
     csv_writer.writerow(["num","PSNR","BPP","MSE",])
-    # test_dataset = CTS_yuv("/home/tione/notebook/datasets",f"{args.test_class}",output_rgb=True)
-    test_dataset = CTS("/home/tione/notebook/datasets",f"{args.test_class}",output_yuv=False)
+    test_dataset = CTS_yuv("/mnt/data-ssd2/shliao/DATA",f"{args.test_class}",output_rgb=True)
+    # test_dataset = CTS("/mnt/data-ssd2/shliao/DATA",f"{args.test_class}",output_yuv=False)
     test_dataloader = DataLoader(test_dataset,batch_size=1,num_workers=1,shuffle=False)
     writer = SummaryWriter(f"{args.save_dir}/plot")
 
@@ -243,6 +252,7 @@ def eval():
     # msg = video_net.cal_macs()
     # print("*"*10,'复杂度',"*"*10,"/n",msg)
     p_model_path = os.path.join(args.p_model,str(args.lmbda), args.model_name)+".pth"
+    # p_model_path = os.path.join(args.p_model, args.model_name)+".pth"
     load_checkpoint = torch.load(p_model_path, map_location=torch.device('cpu'),weights_only=True)
     load_checkpoint_i = torch.load(args.i_model, map_location=torch.device('cpu'),weights_only=True)
     print(f'***************load from : {p_model_path}*************')
